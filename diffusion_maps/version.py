233dea19cbe95829fb17fe30053478c3d2a4000a
Juan M. Bello-Rivas <jmbr@superadditive.com>
2017-05-20 12:39:12 -0400

    Add version file to the repository.

---
+++ diffusion_maps/version.py
@@ -0,0 +1,8 @@
+__all__ = ['version']
+
+
+class version:
+    """Current version."""
+    v_short = '20170520.3'
+    v_long = '20170520.3 (2017-May-20)'
+    v_gnu = 'diffusion_maps 20170520.3 (2017-May-20)'
