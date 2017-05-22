9c3607ac1d62a802fb6b38d5d3ae6ff12ec8c4f5
Juan M. Bello-Rivas <jmbr@superadditive.com>
2017-05-22 06:47:58 -0400

    Update version.

--- diffusion_maps/version.py
+++ diffusion_maps/version.py
@@ -3,6 +3,6 @@ __all__ = ['version']
 
 class version:
     """Current version."""
-    v_short = '20170520.3'
-    v_long = '20170520.3 (2017-May-20)'
-    v_gnu = 'diffusion_maps 20170520.3 (2017-May-20)'
+    v_short = '20170522.5'
+    v_long = '20170522.5 (2017-May-22)'
+    v_gnu = 'diffusion_maps 20170522.5 (2017-May-22)'
