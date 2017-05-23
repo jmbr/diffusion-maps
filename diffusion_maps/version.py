81fb025767e1ddc659530873dcacf0047d678c32
Juan M. Bello-Rivas <jmbr@superadditive.com>
2017-05-23 07:04:14 -0400

    Bump version number.

--- diffusion_maps/version.py
+++ diffusion_maps/version.py
@@ -3,6 +3,6 @@ __all__ = ['version']
 
 class version:
     """Current version."""
-    v_short = '20170522.5'
-    v_long = '20170522.5 (2017-May-22)'
-    v_gnu = 'diffusion_maps 20170522.5 (2017-May-22)'
+    v_short = '20170523.7'
+    v_long = '20170523.7 (2017-May-23)'
+    v_gnu = 'diffusion_maps 20170523.7 (2017-May-23)'
