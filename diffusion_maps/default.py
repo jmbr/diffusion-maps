c5b5186c36b4901607b2085808c9073b11cfdd0c
Juan M. Bello-Rivas <jmbr@superadditive.com>
2017-10-03 06:15:10 -0400

    Automatically disable CUDA in the absence of GPUs.

--- diffusion_maps/default.py
+++ diffusion_maps/default.py
@@ -4,6 +4,11 @@
 
 renormalization = 1             # Renormalization paramter (0 <= alpha <= 1).
 
-num_eigenpairs = 11            # Number of eigenvalue/eigenvectors to obtain.
+num_eigenpairs = 11             # Number of eigenvalue/eigenvectors to obtain.
 
-use_cuda = True                 # Use GPU-accelerated code.
+try:
+    import pycuda.autoinit      # noqa
+except ImportError:
+    use_cuda = False            # Use CPU code.
+else:
+    use_cuda = True             # Use GPU-accelerated code.
